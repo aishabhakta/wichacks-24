e888dd86f9ef0bbe1dd6bbcf3ba94b09b31a621e
aishabhakta <aishabhakta@gmail.com>
2024-03-02 22:18:33 -0500

    html and app js files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import InfoPage from "./pages/InfoPage";
+import ToolsPage from "./pages/ToolsPage";
+import CommunityPage from "./pages/CommunityPage";
+import BottomNav from "./components/BottomNav";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/info" element={<InfoPage />} />
+          <Route path="/tools" element={<ToolsPage />} />
+          <Route path="/community" element={<CommunityPage />} />
+        </Routes>
+        <BottomNav />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
