aec94d03b104d39208dfc1fa95632210ea859a31
Sheila Duong <sheiladuong235@gmail.com>
2024-03-02 23:11:54 -0500

    added title

---
+++ src/pages/ToolsPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography, Box, IconButton } from "@mui/material";
+
+function ToolsPage() {
+    return (
+        <div className="toolsPage">
+            <Typography variant="h4" component="h1" className="tools">
+                Tools
+            </Typography>
+        </div>
+    );
+}
+
+export default ToolsPage;
