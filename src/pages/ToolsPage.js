810206ca55159964b0f16b9d547e7663db53a99c
Sheila Duong <sheiladuong235@gmail.com>
2024-03-03 01:29:59 -0500

    tools subsection titles

--- src/pages/ToolsPage.js
+++ src/pages/ToolsPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Box, IconButton } from "@mui/material";
+import { Typography, Card, CardContent } from "@mui/material";
 
 function ToolsPage() {
     return (
@@ -7,6 +7,51 @@ function ToolsPage() {
             <Typography variant="h4" component="h1" className="tools">
                 Tools
             </Typography>
+            <div className="toolsContainer">
+                <Card className="toolsCard">
+                    <CardContent>
+                        <Typography variant="h4" component="h2" className="subsectionTitle">
+                            Healthcare Locators
+                        </Typography>
+                    </CardContent>
+                </Card>    
+            </div>
+            <div className="toolsContainer">
+                <Card className="toolsCard">
+                    <CardContent>
+                        <Typography variant="h4" component="h2" className="subsectionTitle">
+                            Virtual Consultation
+                        </Typography>
+                    </CardContent>
+                </Card> 
+            </div>
+            <div className="toolsContainer">
+                <Card className="toolsCard">
+                    <CardContent>
+                        <Typography variant="h4" component="h2" className="subsectionTitle">
+                            Sleep Tracker
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
+            <div className="toolsContainer">
+                <Card className="toolsCard">
+                    <CardContent>
+                        <Typography variant="h4" component="h2" className="subsectionTitle">
+                            Breastfeeding Tracker
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
+            <div className="toolsContainer">
+                <Card className="toolsCard">
+                    <CardContent>
+                        <Typography variant="h4" component="h2" className="subsectionTitle">
+                            Due Date Estimator
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
         </div>
     );
 }
